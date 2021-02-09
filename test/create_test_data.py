8cceabe457c55b8de0f850f257a5243e45efa112
Philipp Aumann <philipp.aumann@uibk.ac.at>
2021-02-09 15:36:22 +0100

    Added unittest

---
+++ test/create_test_data.py
@@ -0,0 +1,67 @@
+import os
+import circuitq as cq
+import numpy as np
+import networkx as nx
+
+# =============================================================================
+# LC Circuit
+# =============================================================================
+graph = nx.MultiGraph()
+graph.add_edge(0,1, element = 'C')
+graph.add_edge(0,1, element = 'L')
+circuit = cq.CircuitQ(graph)
+circuit.get_numerical_hamiltonian(200)
+eigv_lc, eigs_lc = circuit.get_eigensystem(30)
+
+# =============================================================================
+# Transmon
+# =============================================================================
+graph = nx.MultiGraph()
+graph.add_edge(0,1, element = 'C')
+graph.add_edge(0,1, element = 'J')
+circuit = cq.CircuitQ(graph, offset_nodes=[1])
+h_num = circuit.get_numerical_hamiltonian(401, grid_length=np.pi*circuit.phi_0)
+eigv_transmon, eigs_transmon = circuit.get_eigensystem(30)
+
+# =============================================================================
+# Fluxonium
+# =============================================================================
+graph = nx.MultiGraph()
+graph.add_edge(0,1, element = 'C')
+graph.add_edge(0,1, element = 'J')
+graph.add_edge(0,1, element = 'L')
+circuit = cq.CircuitQ(graph)
+EJ = circuit.c_v["E"]/2
+phi_ext = np.pi*circuit.phi_0
+h_num = circuit.get_numerical_hamiltonian(401,
+                                parameter_values=[False, EJ, False, phi_ext ])
+eigv_fluxonium, eigs_fluxonium = circuit.get_eigensystem(30)
+
+# =============================================================================
+# Flux Qubit
+# =============================================================================
+graph = nx.MultiGraph()
+graph.add_edge(0,1, element = 'C')
+graph.add_edge(0,1, element = 'J')
+graph.add_edge(1,2, element = 'C')
+graph.add_edge(1,2, element = 'J')
+graph.add_edge(0,2, element = 'C')
+graph.add_edge(0,2, element = 'J')
+circuit = cq.CircuitQ(graph)
+dim = 50
+EJ = 2.5*circuit.c_v["E"]
+alpha = 0.7
+C = circuit.c_v["C"]
+phi_ext = np.pi*circuit.phi_0
+h_num = circuit.get_numerical_hamiltonian(dim,
+                        parameter_values=[C,C,alpha*C,EJ,EJ,alpha*EJ,phi_ext])
+eigv_fluxqubit, eigs_fluxqubit = circuit.get_eigensystem(30)
+
+# =============================================================================
+# Save data
+# =============================================================================
+main_dir = os.path.abspath('..')
+data_path = os.path.join(main_dir, 'test/unittest_data.npy')
+np.save(data_path,
+        [[eigv_lc, eigv_transmon, eigv_fluxonium, eigv_fluxqubit],
+         [eigs_lc, eigs_transmon, eigs_fluxonium, eigs_fluxqubit]])
---
+++ test/test_eigensystem.py
@@ -0,0 +1,32 @@
+import unittest
+import numpy as np
+import os
+
+class TestCircuitQ(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        os.system("python create_test_data.py")
+
+    def setUp(self):
+        main_dir = os.path.abspath('..')
+        data_path = os.path.join(main_dir, 'test/unittest_data.npy')
+        benchmark_data_path = os.path.join(main_dir, 'test/unittest_data_benchmark.npy')
+        self.test_data = np.load(data_path, allow_pickle=True)
+        self.benchmark_data = np.load(benchmark_data_path, allow_pickle=True)
+
+    def test_eigenenergies(self):
+        print("Test eigenenergies of LC-Circuit, Transmon, Fluxoinium and Flux Qubit")
+        for n, evals in enumerate(self.test_data[0]):
+            np.testing.assert_almost_equal(evals, self.benchmark_data[0][n], decimal=25)
+            #self.assertAlmostEqual(evals, self.benchmark_data[0][n], delta=1e-25)
+
+    def test_eigenstates(self):
+        print("Test eigenstates of LC-Circuit, Transmon, Fluxoinium and Flux Qubit")
+        for n, evals in enumerate(self.test_data[1]):
+            np.testing.assert_almost_equal(evals, self.benchmark_data[1][n], decimal=3)
+            # self.assertAlmostEqual(evals, self.benchmark_data[1][n], places=5)
+
+
+if __name__ == '__main__':
+    unittest.main()
